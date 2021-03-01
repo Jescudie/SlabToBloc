a3d077fcef161dbb1345cddcfb1379cc4156c259
Jescudie <57703510+Jescudie@users.noreply.github.com>
2021-03-01 17:33:09 +0100

    mise en place d'un exemple de craft + correctif de compilation

--- src/fr/indes33/slabtobloc/Main.java
+++ src/fr/indes33/slabtobloc/Main.java
@@ -1,17 +1,23 @@
 package fr.indes33.slabtobloc;
 
+import fr.indes33.slabtobloc.Events.Recipes;
 import fr.indes33.slabtobloc.commands.Test;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
+
+    public PluginManager pm = getServer().getPluginManager();
 
     @Override
     public void onEnable() {
         super.onEnable();
         Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Le plugin est allumer");
-        getCommand("test1").setExecutor(new Test());
+        getCommand("test10").setExecutor(new Test());
+        pm.registerEvents(new Recipes(this), this);
     }
 
     @Override
