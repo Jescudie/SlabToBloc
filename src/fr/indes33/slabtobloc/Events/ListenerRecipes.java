238d60587058dfe38767dbad88ac89dbfcbce40f
Jescudie <57703510+Jescudie@users.noreply.github.com>
2021-07-05 21:16:33 +0200

    correction de beug mineurs

--- src/fr/indes33/slabtobloc/Events/ListenerRecipes.java
+++ src/fr/indes33/slabtobloc/Events/ListenerRecipes.java
@@ -77,11 +77,11 @@ public class ListenerRecipes implements Listener {
         SmoothStone.setIngredient('S', Material.SMOOTH_STONE_SLAB);
         this.pl.getServer().addRecipe(SmoothStone);
 
-        NamespacedKey Standstone_key = new NamespacedKey(this.pl, "stb_Standstone");
-        ShapedRecipe Standstone = new ShapedRecipe(Standstone_key, new ItemStack(Material.SANDSTONE, 2));
-        Standstone.shape("SS","SS");
-        Standstone.setIngredient('S', Material.SANDSTONE_SLAB);
-        this.pl.getServer().addRecipe(Standstone);
+        NamespacedKey Sandstone_key = new NamespacedKey(this.pl, "stb_Sandstone");
+        ShapedRecipe Sandstone = new ShapedRecipe(Sandstone_key, new ItemStack(Material.SANDSTONE, 2));
+        Sandstone.shape("SS","SS");
+        Sandstone.setIngredient('S', Material.SANDSTONE_SLAB);
+        this.pl.getServer().addRecipe(Sandstone);
 
         NamespacedKey CutSandstone_key = new NamespacedKey(this.pl, "stb_CutSandstone");
         ShapedRecipe CutSandstone = new ShapedRecipe(CutSandstone_key, new ItemStack(Material.CUT_SANDSTONE, 2));
@@ -115,31 +115,31 @@ public class ListenerRecipes implements Listener {
 
         NamespacedKey NetherBrick_key = new NamespacedKey(this.pl, "stb_NetherBrick");
         ShapedRecipe NetherBrick = new ShapedRecipe(NetherBrick_key, new ItemStack(Material.NETHER_BRICKS, 2));
-        NetherBrick.shape(" SS"," SS","   ");
+        NetherBrick.shape("SS","SS");
         NetherBrick.setIngredient('S', Material.NETHER_BRICK_SLAB);
         this.pl.getServer().addRecipe(NetherBrick);
 
         NamespacedKey Quartz_key = new NamespacedKey(this.pl, "stb_Quartz");
         ShapedRecipe Quartz = new ShapedRecipe(Quartz_key, new ItemStack(Material.QUARTZ_BLOCK, 2));
-        Quartz.shape(" SS"," SS","   ");
+        Quartz.shape("SS","SS");
         Quartz.setIngredient('S', Material.QUARTZ_SLAB);
         this.pl.getServer().addRecipe(Quartz);
 
         NamespacedKey RedSandstone_key = new NamespacedKey(this.pl, "stb_RedSandstone");
         ShapedRecipe RedSandstone = new ShapedRecipe(RedSandstone_key, new ItemStack(Material.RED_SANDSTONE, 2));
-        RedSandstone.shape(" SS"," SS","   ");
+        RedSandstone.shape("SS","SS");
         RedSandstone.setIngredient('S', Material.RED_SANDSTONE_SLAB);
         this.pl.getServer().addRecipe(RedSandstone);
 
         NamespacedKey CutRedSandstone_key = new NamespacedKey(this.pl, "stb_CutRedSandstone");
         ShapedRecipe CutRedSandstone = new ShapedRecipe(CutRedSandstone_key, new ItemStack(Material.CUT_RED_SANDSTONE, 2));
-        CutRedSandstone.shape(" SS"," SS","   ");
+        CutRedSandstone.shape("SS","SS");
         CutRedSandstone.setIngredient('S', Material.CUT_RED_SANDSTONE_SLAB);
         this.pl.getServer().addRecipe(CutRedSandstone);
 
         NamespacedKey Purpur_key = new NamespacedKey(this.pl, "stb_Purpur");
         ShapedRecipe Purpur = new ShapedRecipe(Purpur_key, new ItemStack(Material.PURPUR_BLOCK, 2));
-        Purpur.shape(" SS"," SS","   ");
+        Purpur.shape("SS","SS");
         Purpur.setIngredient('S', Material.PURPUR_SLAB);
         this.pl.getServer().addRecipe(Purpur);
 
@@ -151,109 +151,109 @@ public class ListenerRecipes implements Listener {
 
         NamespacedKey PrismarineBricks_key = new NamespacedKey(this.pl, "stb_PrismarineBricks");
         ShapedRecipe PrismarineBricks = new ShapedRecipe(PrismarineBricks_key, new ItemStack(Material.PRISMARINE_BRICKS, 2));
-        PrismarineBricks.shape(" SS"," SS","   ");
+        PrismarineBricks.shape("SS","SS");
         PrismarineBricks.setIngredient('S', Material.PRISMARINE_BRICK_SLAB);
         this.pl.getServer().addRecipe(PrismarineBricks);
 
         NamespacedKey DarkPrismarine_key = new NamespacedKey(this.pl, "stb_DarkPrismarine");
         ShapedRecipe DarkPrismarine = new ShapedRecipe(DarkPrismarine_key, new ItemStack(Material.DARK_PRISMARINE, 2));
-        DarkPrismarine.shape(" SS"," SS","   ");
+        DarkPrismarine.shape("SS","SS");
         DarkPrismarine.setIngredient('S', Material.DARK_PRISMARINE_SLAB);
         this.pl.getServer().addRecipe(DarkPrismarine);
 
         NamespacedKey PolishedGranite_key = new NamespacedKey(this.pl, "stb_PolishedGranite");
         ShapedRecipe PolishedGranite = new ShapedRecipe(PolishedGranite_key, new ItemStack(Material.POLISHED_GRANITE, 2));
-        PolishedGranite.shape(" SS"," SS","   ");
+        PolishedGranite.shape("SS","SS");
         PolishedGranite.setIngredient('S', Material.POLISHED_GRANITE_SLAB);
         this.pl.getServer().addRecipe(PolishedGranite);
 
         NamespacedKey SmoothRedSandstone_key = new NamespacedKey(this.pl, "stb_SmoothRedSandstone");
         ShapedRecipe SmoothRedSandstone = new ShapedRecipe(SmoothRedSandstone_key, new ItemStack(Material.SMOOTH_RED_SANDSTONE, 2));
-        SmoothRedSandstone.shape(" SS"," SS","   ");
+        SmoothRedSandstone.shape("SS","SS");
         SmoothRedSandstone.setIngredient('S', Material.SMOOTH_RED_SANDSTONE_SLAB);
         this.pl.getServer().addRecipe(SmoothRedSandstone);
 
         NamespacedKey MossyStoneBrick_key = new NamespacedKey(this.pl, "stb_MossyStoneBrick");
         ShapedRecipe MossyStoneBrick = new ShapedRecipe(MossyStoneBrick_key, new ItemStack(Material.MOSSY_STONE_BRICKS, 2));
-        MossyStoneBrick.shape(" SS"," SS","   ");
+        MossyStoneBrick.shape("SS","SS");
         MossyStoneBrick.setIngredient('S', Material.MOSSY_STONE_BRICK_SLAB);
         this.pl.getServer().addRecipe(MossyStoneBrick);
 
         NamespacedKey PolishedDiorite_key = new NamespacedKey(this.pl, "stb_PolishedDiorite");
         ShapedRecipe PolishedDiorite = new ShapedRecipe(PolishedDiorite_key, new ItemStack(Material.POLISHED_DIORITE, 2));
-        PolishedDiorite.shape(" SS"," SS","   ");
+        PolishedDiorite.shape("SS","SS");
         PolishedDiorite.setIngredient('S', Material.POLISHED_DIORITE_SLAB);
         this.pl.getServer().addRecipe(PolishedDiorite);
 
         NamespacedKey MossyCobblestone_key = new NamespacedKey(this.pl, "stb_MossyCobblestone");
         ShapedRecipe MossyCobblestone = new ShapedRecipe(MossyCobblestone_key, new ItemStack(Material.MOSSY_COBBLESTONE, 2));
-        MossyCobblestone.shape(" SS"," SS","   ");
+        MossyCobblestone.shape("SS","SS");
         MossyCobblestone.setIngredient('S', Material.MOSSY_COBBLESTONE_SLAB);
         this.pl.getServer().addRecipe(MossyCobblestone);
 
         NamespacedKey EndStoneBrick_key = new NamespacedKey(this.pl, "stb_EndStoneBrick");
         ShapedRecipe EndStoneBrick = new ShapedRecipe(EndStoneBrick_key, new ItemStack(Material.END_STONE, 2));
-        EndStoneBrick.shape(" SS"," SS","   ");
+        EndStoneBrick.shape("SS","SS");
         EndStoneBrick.setIngredient('S', Material.END_STONE_BRICK_SLAB);
         this.pl.getServer().addRecipe(EndStoneBrick);
 
         NamespacedKey SmoothSandstone_key = new NamespacedKey(this.pl, "stb_SmoothSandstone");
         ShapedRecipe SmoothSandstone = new ShapedRecipe(SmoothSandstone_key, new ItemStack(Material.SMOOTH_SANDSTONE, 2));
-        SmoothSandstone.shape(" SS"," SS","   ");
+        SmoothSandstone.shape("SS","SS");
         SmoothSandstone.setIngredient('S', Material.SMOOTH_SANDSTONE_SLAB);
         this.pl.getServer().addRecipe(SmoothSandstone);
 
         NamespacedKey SmoothQuartz_key = new NamespacedKey(this.pl, "stb_SmoothQuartz");
         ShapedRecipe SmoothQuartz = new ShapedRecipe(SmoothQuartz_key, new ItemStack(Material.SMOOTH_QUARTZ, 2));
-        SmoothQuartz.shape(" SS"," SS","   ");
+        SmoothQuartz.shape("SS","SS");
         SmoothQuartz.setIngredient('S', Material.SMOOTH_QUARTZ_SLAB);
         this.pl.getServer().addRecipe(SmoothQuartz);
 
         NamespacedKey Granite_key = new NamespacedKey(this.pl, "stb_Granite");
         ShapedRecipe Granite = new ShapedRecipe(Granite_key, new ItemStack(Material.GRANITE, 2));
-        Granite.shape(" SS"," SS","   ");
+        Granite.shape("SS","SS");
         Granite.setIngredient('S', Material.GRANITE_SLAB);
         this.pl.getServer().addRecipe(Granite);
 
         NamespacedKey Andesite_key = new NamespacedKey(this.pl, "stb_Andesite");
         ShapedRecipe Andesite = new ShapedRecipe(Andesite_key, new ItemStack(Material.ANDESITE, 2));
-        Andesite.shape(" SS"," SS","   ");
+        Andesite.shape("SS","SS");
         Andesite.setIngredient('S', Material.ANDESITE_SLAB);
         this.pl.getServer().addRecipe(Andesite);
 
         NamespacedKey RedNetherBrick_key = new NamespacedKey(this.pl, "stb_RedNetherBrick");
         ShapedRecipe RedNetherBrick = new ShapedRecipe(RedNetherBrick_key, new ItemStack(Material.RED_NETHER_BRICKS, 2));
-        RedNetherBrick.shape(" SS"," SS","   ");
+        RedNetherBrick.shape("SS","SS");
         RedNetherBrick.setIngredient('S', Material.RED_NETHER_BRICK_SLAB);
         this.pl.getServer().addRecipe(RedNetherBrick);
 
         NamespacedKey PolishedAndesite_key = new NamespacedKey(this.pl, "stb_PolishedAndesite");
         ShapedRecipe PolishedAndesite = new ShapedRecipe(PolishedAndesite_key, new ItemStack(Material.POLISHED_ANDESITE, 2));
-        PolishedAndesite.shape(" SS"," SS","   ");
+        PolishedAndesite.shape("SS","SS");
         PolishedAndesite.setIngredient('S', Material.POLISHED_ANDESITE_SLAB);
         this.pl.getServer().addRecipe(PolishedAndesite);
 
         NamespacedKey Diorite_key = new NamespacedKey(this.pl, "stb_Diorite");
         ShapedRecipe Diorite = new ShapedRecipe(Diorite_key, new ItemStack(Material.DIORITE, 2));
-        Diorite.shape(" SS"," SS","   ");
+        Diorite.shape("SS","SS");
         Diorite.setIngredient('S', Material.DIORITE_SLAB);
         this.pl.getServer().addRecipe(Diorite);
 
         NamespacedKey BlackStone_key = new NamespacedKey(this.pl, "stb_BlackStone");
         ShapedRecipe BlackStone = new ShapedRecipe(BlackStone_key, new ItemStack(Material.BLACKSTONE, 2));
-        BlackStone.shape(" SS"," SS","   ");
+        BlackStone.shape("SS","SS");
         BlackStone.setIngredient('S', Material.BLACKSTONE_SLAB);
         this.pl.getServer().addRecipe(BlackStone);
 
         NamespacedKey PolishedBlackStone_key = new NamespacedKey(this.pl, "stb_PolishedBlackStone");
         ShapedRecipe PolishedBlackStone = new ShapedRecipe(PolishedBlackStone_key, new ItemStack(Material.POLISHED_BLACKSTONE, 2));
-        PolishedBlackStone.shape(" SS"," SS","   ");
+        PolishedBlackStone.shape("SS","SS");
         PolishedBlackStone.setIngredient('S', Material.POLISHED_BLACKSTONE_SLAB);
         this.pl.getServer().addRecipe(PolishedBlackStone);
 
         NamespacedKey PolishedBlackStoneBrick_key = new NamespacedKey(this.pl, "stb_PolishedBlackStoneBrick");
         ShapedRecipe PolishedBlackStoneBrick = new ShapedRecipe(PolishedBlackStoneBrick_key, new ItemStack(Material.POLISHED_BLACKSTONE_BRICKS, 2));
-        PolishedBlackStoneBrick.shape(" SS"," SS","   ");
+        PolishedBlackStoneBrick.shape("SS","SS");
         PolishedBlackStoneBrick.setIngredient('S', Material.POLISHED_BLACKSTONE_BRICK_SLAB);
         this.pl.getServer().addRecipe(PolishedBlackStoneBrick);
     }
